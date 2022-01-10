8f25d2af82173653158698be813c4ba9698bd7aa
GorchArtem <rick-flat@mail.ru>
2022-01-10 03:44:13 +0300

    add DemowebshopTests with UI steps

--- src/test/java/DemowebshopTests.java
+++ src/test/java/DemowebshopTests.java
@@ -1,12 +1,14 @@
 import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
-import org.apiguardian.api.API;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
@@ -25,9 +27,11 @@ public class DemowebshopTests {
     void checkCustomersInfo() {
         String login = "artem@qaguru.tr";
         String password = "Artem1349";
+//        SelenideElement customersField = $(".fieldset");
 
         step("Get cookie and set it to browser by API", () -> {
-            String authorizationCookie = given()
+            String authorizationCookie =
+                    given()
                     .contentType("application/x-www-form-urlencoded")
                     .formParam("Email", login)
                     .formParam("Password", password)
@@ -45,5 +49,15 @@ public class DemowebshopTests {
                     getWebDriver().manage().addCookie(
                             new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
         });
+        step("Open Customer Info", () ->
+                open("http://demowebshop.tricentis.com/customer/info"));
+
+        step("Check firstname", () ->
+                $("#FirstName").shouldHave(value("Artem")));
+        step("Check lastname", () ->
+                $("#LastName").shouldHave(value("Gorchakov")));
+        step("Check email", () ->
+                $("#Email").shouldHave(value(login)));
+
     }
 }
