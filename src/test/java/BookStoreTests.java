81f148c856d497e3f7b3c6770640cf4f41f056ff
GorchArtem <rick-flat@mail.ru>
2022-01-15 15:27:28 +0300

    add BookStoreTests

---
+++ src/test/java/BookStoreTests.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+
+public class BookStoreTests {
+
+    @Test
+    void simpleGetTest() {
+        given()
+                .get("https://demoqa.com/BookStore/v1/Books")
+                .then()
+                .body("books", hasSize(greaterThan(0)));
+    }
+}
